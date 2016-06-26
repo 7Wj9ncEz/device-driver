06c0fd91d800dfd3816df69f024194325470b66d
Igor Ribeiro Barbosa Duarte <igor.ribeiro.duarte@gmail.com>
2016-06-26 18:16:46 -0300

    Second version

--- usb_driver.c
+++ usb_driver.c
@@ -2,12 +2,27 @@
 #include <linux/kernel.h>
 #include <linux/usb.h>
 
-static struct usb_device_id pen_table[]={
-	{USB_DEVICE(0x0781, 0x5406)},
-	{}
+//probe function
+//called on device insertion if and only if no other driver has beat us to the punch
+static int pen_probe(struct usb_interface *interface, const struct usb_device_id *id){
+	printk(KERN_INFO "[*] FSO Pen drive (%04X:%04X) plugged\n", id->idVendor, id->idProduct);
+	return 0; //return 0 indicates we will manage this device
+}
+
+//disconnect
+static void pen_disconnect(struct usb_interface *interface){
+	printk(KERN_INFO "[*] Pen drive removed\n");
+}
+
+//usb_device_id
+static struct usb_device_id pen_table[] = {
+	//0781:5406
+	{USB_DEVICE(0x0781, 0x5406)}, //information is obtained using "]susb" at the command line
+	{} /* Terminating entry */
 }
 
 MODULE_DEVICE_TABLE (USB, pen_table);
+
 static struct usb_driver pen_driver = 
 {
 	.name = "USB-Driver",
@@ -18,6 +33,12 @@ static struct usb_driver pen_driver =
 
 static int __init pen_init(void){
 	int ret=-1;
+	
+	printk(KERN_INFO "[*]Constructor of driver");
+	printk(KERN_INFO "\tRegistering Driver with Kernel");
+	ret = usb_register(&pen_driver);
+	printk(KERN_INFO "\nRegistration is complet");
+
 	return ret;
 }
 
