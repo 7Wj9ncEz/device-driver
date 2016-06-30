ad9d3eb5c02833b0b992af6011e54e95ae079861
Vítor Barbosa <vitornga15@gmail.com>
2016-06-29 21:24:35 -0300

    Improving makefile and program to write on device file

--- char-driver/writing_in_device.c
+++ char-driver/writing_in_device.c
@@ -6,19 +6,29 @@
 
 int main(int argc, char *argv[]){
 	char buf[100];
+    char device_filename[100] = "/dev/char_device";
 	int fp;
 
-	assert(argc > 1);
+    if(argc < 2){
+        printf("Usage: %s \"message\"\n", argv[0]);
+        return -1;
+    }
+
 	memset(buf, 0, 100);
-	printf("Input: %s\n", argv[1]);
 
-	fp = open("/dev/char_device", O_RDWR);
+    printf("Opening device file [%s]\n\n", device_filename);
 
+	fp = open(device_filename, O_RDWR);
+
+    printf("Writing [%s] in [%s]...\n", argv[1], device_filename);
 	write(fp, argv[1], strlen(argv[1]));
+    printf("Writing successful\n\n");
 
+    printf("Reading from [%s]...\n", device_filename);
 	while(read(fp, buf, 128));
+	printf("Reading succesful!\n");
 
-	printf("Message: %s\n", buf);
+    printf("\nMessage read: [%s]\n", buf);
 
 	return 0;
 }
